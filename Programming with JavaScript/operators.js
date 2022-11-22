b4a954fcdbfd37c735a7e732c813bd12bc638687
Kamar Ko <78088987+kamarkor@users.noreply.github.com>
2022-11-22 19:23:22 +0100

    Add operators

---
+++ Programming with JavaScript/operators.js
@@ -0,0 +1,25 @@
+/* Arithmetic operators in JS:
+    + addition
+    - subtraction
+    * multiplication
+    / division
+*/
+
+/* Comparison operators in JS:
+    > greater than
+    < less than
+    == equality
+    === strict equality
+    != inequality
+    !== strict inequality
+    += addition/concatenation assignment
+*/
+
+console.log(2 + 2); //4
+console.log(1 + 2 + 3 + 4 + 5); //15
+console.log(20 - 18); //2
+console.log(2 * 3); //6
+console.log(8 / 1); //8
+console.log(3 > 2); //true
+console.log(2 > 3); //false
+console.log(10 == 10); //true
