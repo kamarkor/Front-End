8cc321023997fb07d370bc31805742025f7d2f00
Kamar Ko <78088987+kamarkor@users.noreply.github.com>
2023-01-02 13:13:06 +0100

    Delete jest directory

--- jest/timesTwo.js
+++
@@ -1,6 +0,0 @@
-// Task 1: Code the timesTwo function declaration
-function timesTwo(number){
-    return number * 2;
-}
-// Task 2: Export the timesTwo function as a module
-module.exports = timesTwo;
